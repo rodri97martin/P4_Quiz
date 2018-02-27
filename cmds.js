5759cd27d64c63320b96728123e77c12cdf78ac8
Rodrigo Martín Martín <Rodri@MacBook-Pro-de-Rodrigo.local>
2018-02-27 13:21:27 +0100

    Sin metodo play

---
+++ cmds.js
@@ -0,0 +1,122 @@
+const model = require('./model');
+const {log, biglog, colorize, errorlog} = require('./out');
+
+exports.helpCmd = rl => {
+    log("Comandos:");
+    log("   h|help - Muestra esta ayuda.");
+    log("   list - Listar los quizzes existentes.");
+    log("   show <id> - Muestra la pregunda y la respuesta del quiz indicado.");
+    log("   add - Añadir un nuevo quiz interactivamente.");
+    log("   delete <id> - Borrar el quiz indicado.");
+    log("   edit <id> - Editar el quiz indicado.");
+    log("   test <id> - Probar el quiz indicado.");
+    log("   p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+    log("   credits - Créditos.");
+    log("   q|quit - Salir del programa.");
+};
+
+exports.quitCmd = rl => {
+    rl.close();
+};
+exports.addCmd = rl => {
+
+    rl.question(colorize('Introduzca una nueva pregunta: ', 'red'), question => {
+
+        rl.question(colorize('Introduzca la respuesta: ', 'red'), answer => {
+
+            model.add(question, answer);
+            log(`   ${colorize('Se ha añadido', 'magenta')}: ${question} ${colorize('=>', 'magenta')} ${answer}`);
+            rl.prompt();
+        });
+    });
+};
+exports.listCmd = rl => {
+    model.getAll().forEach((quiz, id) => {
+        log(`   [${colorize(id, 'magenta')}]: ${quiz.question}`);
+    });
+    rl.prompt();
+};
+exports.showCmd = (rl, id) => {
+
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+    } else {
+        try {
+            const quiz = model.getByIndex(id);
+            log(`   [${colorize(id, 'magenta')}]: ${quiz.question} ${colorize('=>', 'magenta')} ${quiz.answer}`);
+        } catch (error) {
+            errorlog(error.message);
+        }
+    }
+    rl.prompt();
+};
+exports.testCmd = (rl, id) => {
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+        rl.prompt();
+    } else {
+        try {
+            const quiz = model.getByIndex(id);
+            rl.question(colorize(`${quiz.question}: `, 'red'), answer =>{
+                if (answer === quiz.answer){
+                    biglog("Correcta", "green");
+                } else {
+                    biglog("Incorrecta", "red");
+                }
+                rl.prompt();
+            });
+
+        } catch (error) {
+            errorlog(error.message);
+            rl.prompt();
+        }
+    }
+};
+exports.playCmd = rl => {
+    log("   p|play - Jugar a preguntar aleatoriamente todos los quizzes.", "red");
+    rl.prompt();
+};
+exports.deleteCmd = (rl, id) => {
+
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+    } else {
+        try {
+            model.deleteByIndex(id);
+        } catch (error) {
+            errorlog(error.message);
+        }
+    }
+    rl.prompt();
+};
+exports.editCmd = (rl, id) => {
+
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+        rl.prompt();
+    } else {
+        try {
+            const quiz = model.getByIndex(id);
+            process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)}, 0);
+
+            rl.question(colorize('Introduzca una pregunta: ', 'red'), question => {
+
+                process.stdout.isTTY && setTimeout(() => {rl.write(quiz.answer)}, 0);
+
+                rl.question(colorize('Introduzca la respuesta: ', 'red'), answer => {
+                    model.update(id, question, answer);
+                    log(`   Se ha cambiado el quiz ${colorize(id, 'magenta')} por: ${question} ${colorize('=>', 'magenta')} ${answer}`);
+                    rl.prompt();
+                });
+            });
+        } catch (error) {
+            errorlog(error.message);
+            rl.prompt();
+        }
+    }
+};
+exports.creditsCmd = rl => {
+    log("Autor de la práctica:");
+    log("Rodrigo Martín Martín", "green");
+    rl.prompt();
+};
